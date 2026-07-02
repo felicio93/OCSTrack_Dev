fc5617302509afbd7ff39fbe88b84fa779dc0cfc
felicio93 <felicio.cassalho@noaa.gov>
2026-07-02 16:45:21 +0000

    Fix: Add missing parenthesis to logger calls

--- ocstrack/Model/model.py
+++ ocstrack/Model/model.py
@@ -682,9 +682,9 @@ class WW3:
                 continue
 
         if not selected:
-            _logger.warning(f"No files matched pattern in {self.output_dir}.
-"
+            _logger.warning(f"No files matched pattern in {self.output_dir}.\n"
             f"Make sure the model files fall within {self.start_date} and {self.end_date} ")
+        return selected
         return selected
 
     def _load_mesh_data(self, filepath: str) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
